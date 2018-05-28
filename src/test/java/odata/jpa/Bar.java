77097679f107c236528624ea8ff199f0a5f39b83
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-05-28 11:00:21 +0200

    tests

---
+++ src/test/java/odata/jpa/Bar.java
@@ -0,0 +1,39 @@
+/*
+* WebTemplate 1.0
+* Luca Vercelli 2017
+* Released under MIT license 
+*/
+package odata.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EXAMPLE_FOO")
+public class Bar {
+
+	private Long id;
+	private String description;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ src/test/java/odata/jpa/TestOdata.java
@@ -0,0 +1,14 @@
+package odata.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOdata {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
