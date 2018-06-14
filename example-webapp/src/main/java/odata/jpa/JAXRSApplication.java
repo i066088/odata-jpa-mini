129be5540999a50bd71538ec5b30c89620c4c979
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-06-14 16:53:30 +0200

    split file

--- example-webapp/src/main/java/odata/jpa/JAXRSApplication.java
+++ example-webapp/src/main/java/odata/jpa/JAXRSApplication.java
@@ -5,24 +5,12 @@
 */
 package odata.jpa;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 /**
  * Configures a JAX-RS endpoint.
- *
- * @author airhacks.com
  */
 @ApplicationPath("/resources.svc")
-public class JAXRSApplication extends Application {
-
-	@Override
-	public Map<String, Object> getProperties() {
-		Map<String, Object> props = new HashMap<>();
-		props.put("jersey.config.server.provider.classnames", "org.glassfish.jersey.media.multipart.MultiPartFeature");
-		return props;
-	}
+public class JAXRSApplication extends AbstractJAXRSApplication {
+	
 }
---
+++ odata-jpa-mini/src/main/java/odata/jpa/AbstractJAXRSApplication.java
@@ -0,0 +1,21 @@
+package odata.jpa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint.
+ * 
+ * Webapps must extend this.
+ */
+public abstract class AbstractJAXRSApplication extends Application {
+
+	public Map<String, Object> getProperties() {
+		Map<String, Object> props = new HashMap<>();
+		props.put("jersey.config.server.provider.classnames", "org.glassfish.jersey.media.multipart.MultiPartFeature");
+		return props;
+	}
+
+}
