aa1a9364b39801a912dc73b8d043f0c3d32be4b1
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-06-15 21:53:35 +0200

    clean

--- odata-jpa-mini/src/main/java/odata/jpa/RestResourcesEndpoint.java
+++ odata-jpa-mini/src/main/java/odata/jpa/RestResourcesEndpoint.java
@@ -9,12 +9,10 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
